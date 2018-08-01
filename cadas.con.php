1bec633a1639dacc0a74ff6658ff02ee1a52b910
Dimitri Rocha <dimitrirocha10@gmail.com>
2018-08-01 20:26:53 -0300

    Não sei oque foi alterado

--- cadas.con.php
+++ cadas.con.php
@@ -1,14 +1,14 @@
 <?php include '_config/debug.php'; ?>
-<?php 
+<?php
   require_once '_class/conexao.php';
   require_once '_ref/header.php';
-  
+
   if($login->isloginadm()){
   }else{ header('adm.log.php');
   }
 
 	if(isset($_POST['btn-enviar'])){
-    
+
 		$cname= $_POST['name'];
 		$cpass= $_POST['pass'];
 		$cmail= $_POST['mail'];
@@ -24,7 +24,7 @@
       $pdo = $DB_con->prepare("SELECT con_name, con_mail FROM consultor WHERE con_name=:cname OR con_mail=:cmail");
       $pdo->execute(array(':cname'=>$cname, ':cmail'=>$cmail));
       $row=$pdo->fetch(PDO::FETCH_ASSOC);
-    
+
       if($row['con_name']==$cname){
         echo "JÁ TEMOS ESSE CONSULTOR";
       }
@@ -44,17 +44,17 @@
  	<form method="post">
 
  		<input required type="text" name="name" placeholder="Login"><br>
- 		
+
     <input required type="email" name="mail" placeholder="Email"><br>
 
  		<input required type="password" name="pass" placeholder="Senha" ><br>
- 		
+
  		<input required type="number" name="vall" placeholder="Valor por msg"><br>
- 		
+
  		<input required="Qual sua área de atuação?" type="text" name="atua" placeholder="Área de atuação"><br>
- 		
+
  		<input required type="text" name="addr" placeholder="Enrereço"><br>
- 		
+
  		<button type="submit" name="btn-enviar" >Enviar</button>
 
  	</form>
@@ -62,6 +62,6 @@
  </div>
 </section>
 
-<?php 
+<?php
 	require_once '_ref/footer.php';
 ?>
